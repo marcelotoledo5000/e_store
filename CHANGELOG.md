eb5f9bc1e2c6b3e5148e1297db56f4ca701900d0
Marcelo Toledo <marcelotoledo5000@gmail.com>
2019-04-20 19:07:54 -0300

    Fixed CodeClimate config

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,18 @@
-# e_store 0.0.1 (Apr 19, 2019)
+# e_store 0.0.3 (Apr 20, 2019)
+
+* TDD: Added tests to CRUD of Customer Model
+* Fixed CodeClimate config
+
+  *Marcelo Toledo*
+
+## e_store 0.0.2 (Apr 19, 2019)
 
 * TDD: Added tests to CRUD of Product Model
 * Added CodeClimate config
 
   *Marcelo Toledo*
 
-## e_store 0.0.0 (Apr 19, 2019)
+## e_store 0.0.1 (Apr 19, 2019)
 
 * Created project
 * Configured project
--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,12 +2,12 @@ module ExceptionHandler
   extend ActiveSupport::Concern
 
   included do
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
+    rescue_from ActiveRecord::RecordNotFound do |error|
+      json_response({ message: error.message }, :not_found)
     end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
+    rescue_from ActiveRecord::RecordInvalid do |error|
+      json_response({ message: error.message }, :unprocessable_entity)
     end
   end
 end

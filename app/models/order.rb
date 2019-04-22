5021a16f7c9c6bc7ed3ad7392082af6d954c61b5
Marcelo Toledo <marcelotoledo5000@gmail.com>
2019-04-22 02:18:55 -0300

    Added new validations and relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :items, dependent: :destroy
+
+  validates :freight, :items, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  it { should have_many(:items) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:stock) }
